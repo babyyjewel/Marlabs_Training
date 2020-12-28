922b761798267cbc4cd31a69635c4e6aeae75374
babyyjewel <41678496+babyyjewel@users.noreply.github.com>
2020-12-28 21:22:39 +0530

    change

---
+++ switchhh.java
@@ -0,0 +1,14 @@
+class switchhh{
+  public static void main(String args[]){
+           Integer choice = 2;
+          switch(choice)
+	   {        
+   		case 1 :  System.out.println("Hi");
+ 				break;
+		case 2 :  System.out.println("Hello");
+ 				break; 
+		case 3 :  System.out.println("Bye");
+ 				break;
+           }
+}
+}
