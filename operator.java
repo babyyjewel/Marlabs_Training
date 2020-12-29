a45eabcc019aaf1c7cd5ed7e64af2f6412541cc7
babyyjewel <41678496+babyyjewel@users.noreply.github.com>
2020-12-29 16:11:44 +0530

    change

---
+++ operator.java
@@ -0,0 +1,31 @@
+class operator{
+      
+ public static void main(String args[])
+{         int a=1;
+            int b=2;
+        System.out.println(a+b);
+         System.out.println(b-a);
+       System.out.println(a*b);
+    System.out.println(b/a);
+    System.out.println(a%b);
+         System.out.println(a++);
+	System.out.println(++a);
+       System.out.println(a--);
+        System.out.println(--a);
+	System.out.println(a+b);
+       System.out.println(a<<b);
+       System.out.println(a>>b);
+         System.out.println(a+b & b-a);
+         System.out.println(a<b && b>a);
+       
+         System.out.println(a>b && b>a);
+         System.out.println(a>b && b<a);
+          System.out.println((a<b)?a:b);
+System.out.println(a+=1);
+System.out.println(a-=2);         
+System.out.println(a==1);
+System.out.println(a!=1);  
+  System.out.println(a>b || b<a);                
+                
+}
+}
