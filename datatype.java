ecd12197e9f8b43ae4c63677442ff14563b7f259
babyyjewel <41678496+babyyjewel@users.noreply.github.com>
2020-12-29 16:19:27 +0530

    change

---
+++ datatype.java
@@ -0,0 +1,34 @@
+class datatype{
+      
+ public static void main(String args[])
+{      Integer a = 3; 
+         System.out.println(" integer");   
+        System.out.println(a.BYTES);
+        System.out.println(a.SIZE);
+        System.out.println(a.MAX_VALUE);
+         System.out.println(a.MIN_VALUE + "\n");
+
+	Float b = 1.23f;
+         System.out.println("float");  
+    System.out.println(b.BYTES );
+        System.out.println(b.SIZE);
+        System.out.println(b.MAX_VALUE);
+         System.out.println(b.MIN_VALUE + "\n");
+
+
+         Byte c = 1;
+         System.out.println(" byte");  
+    System.out.println(c.BYTES );
+        System.out.println(c.SIZE);
+        System.out.println(c.MAX_VALUE);
+         System.out.println(c.MIN_VALUE + "\n");
+
+       Double  d = 1.678888;
+      System.out.println("double");  
+       System.out.println(d.BYTES );
+        System.out.println(d.SIZE);
+        System.out.println(d.MAX_VALUE);
+         System.out.println(d.MIN_VALUE + "\n");
+
+}
+}
