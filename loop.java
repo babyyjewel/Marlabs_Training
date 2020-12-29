2c57aa669ee403bae5bb8c76109fcb733783e1cb
babyyjewel <41678496+babyyjewel@users.noreply.github.com>
2020-12-29 16:13:15 +0530

    change

---
+++ loop.java
@@ -0,0 +1,20 @@
+class loop{
+  public static void main(String args[]){
+        int i=0;
+       System.out.println("Demo");
+       for(i=0;i<10;i++)
+       {   System.out.println("Hi");
+       }
+int k=0;
+       do{          
+            System.out.println("Hello");
+               k++;
+          }while(k<10);
+
+int l=0;       
+ while(l<10){ 
+              System.out.println("morning"); l++;
+             }
+
+                                          }
+}
