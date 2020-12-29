7cda1498d2274020205de59584cb1c7166360f8f
babyyjewel <41678496+babyyjewel@users.noreply.github.com>
2020-12-29 16:08:11 +0530

    change

---
+++ switchh.java
@@ -0,0 +1,14 @@
+class switchh{
+  public static void main(String args[]){
+           String choice = "hi";
+          switch(choice)
+	   {        
+   		case "hi" :  System.out.println("Hi");
+ 				break;
+		case "hello" :  System.out.println("Hello");
+ 				break; 
+		case "bye" :  System.out.println("Bye");
+ 				break;
+           }
+}
+}
