b1d73d561b38321f221d3b2a401e9c601a2d422d
babyyjewel <41678496+babyyjewel@users.noreply.github.com>
2020-12-29 16:22:10 +0530

    change

---
+++ Test.java
@@ -0,0 +1,17 @@
+ public class Test {  
+
+   public static void main(String[] args) {  
+
+       int count = 1;  
+
+      while (count <= 15) {  
+
+       System.out.println(count % 2 == 1 ? "***" : "+++++");  
+
+       ++count;  
+
+          }      // end while  
+
+      }       // end main   
+
+    }  
